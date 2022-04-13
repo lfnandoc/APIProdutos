241620f52306c86ec73ea20e179702cde6983bde
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-13 12:06:06 -0300

    add repository code

---
+++ IntercambioGenebra_API/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using IntercambioGenebraAPI.Entities;
+using IntercambioGenebraAPI.Infra;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntercambioGenebraAPI.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Category?> GetCategoryByIdAsync(Guid id)
+        {
+            var category = await _context
+                .Categories
+                .FirstOrDefaultAsync(category => category.Id == id);
+            
+            return category;            
+        }
+        
+        public async Task<List<Category>> GetAllCategories()
+        {
+            var categories = await _context
+                .Categories
+                .AsNoTracking()
+                .ToListAsync();
+
+            return categories;
+        }
+
+        public async Task<bool> CategoryHasAssociatedProducts(Guid id)
+        {
+            var anyAssociatedProducts = await _context
+                .Products
+                .AsNoTracking()
+                .AnyAsync(product => product.CategoryId == id);
+
+            return anyAssociatedProducts;
+        }
+
+    }
+}
---
+++ IntercambioGenebra_API/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using IntercambioGenebraAPI.Entities;
+using IntercambioGenebraAPI.Infra;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntercambioGenebraAPI.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Product?> GetProductByIdAsync(Guid id)
+        {
+            var product = await _context
+                .Products
+                .FirstOrDefaultAsync(product => product.Id == id);
+            
+            return product;            
+        }
+        public async Task<List<Product>> GetAllCategoriesAsync()
+        {
+            var products = await _context
+                .Products
+                .AsNoTracking()
+                .ToListAsync();
+
+            return products;
+        }
+
+    }
+}
