bc80159aa89dbcc44832bf9934da8fe12074d79f
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-13 11:25:17 -0300

    changes id to guid

--- IntercambioGenebra_API/Entities/Entity.cs
+++ IntercambioGenebra_API/Entities/Entity.cs
@@ -1,7 +1,12 @@
-﻿namespace IntercambioGenebraAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntercambioGenebraAPI.Entities
 {
     public class Entity
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
     }
 }
