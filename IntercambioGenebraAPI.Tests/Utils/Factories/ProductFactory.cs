9bf6cf80560bb990f4ba6e8aad83af4338d89cd7
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-19 11:43:08 -0300

    implements product factory

---
+++ IntercambioGenebraAPI.Tests/Utils/Factories/ProductFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntercambioGenebraAPI.Domain.Entities;
+using IntercambioGenebraAPI.Domain.Repositories;
+
+namespace IntercambioGenebraAPI.Tests.Utils.Factories
+{
+    public static class ProductFactory
+    {
+        public static Product CreateTestProduct(IProductRepository _productRepository, ICategoryRepository _categoryRepository)
+        {
+            const string testProductName = "";
+            const decimal testProductPrice = 10.85m;
+            var testCategory = CategoryFactory.CreateTestCategory(_categoryRepository);
+            var testProduct = new Product(testProductName, testCategory, testProductPrice);
+            
+            _productRepository.Insert(testProduct);
+            _productRepository.Save();
+
+            return testProduct;
+        }
+    }
+}
