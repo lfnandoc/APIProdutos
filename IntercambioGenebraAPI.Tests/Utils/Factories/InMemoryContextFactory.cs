63e7e13d6471d06b5231086b3c83e012d2e322f1
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-19 10:00:59 -0300

    implements unit tests project

---
+++ IntercambioGenebraAPI.Tests/Utils/Factories/InMemoryContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using IntercambioGenebraAPI.Infrastructure;
+
+namespace IntercambioGenebraAPI.Tests.Utils.Factories
+{
+    public static class InMemoryContextFactory
+    {
+        public static AppDbContext Create()
+        {
+            var databaseName = Guid.NewGuid().ToString();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+            
+            return new AppDbContext(options);
+        }
+    }
+}
