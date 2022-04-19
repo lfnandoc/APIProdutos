91473e4ec74b41920f6970ade73264c89e1276e2
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-19 15:33:31 -0300

    implements query unit tests

---
+++ IntercambioGenebraAPI.Tests/Application/Queries/GetAllCategoriesQueryHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IntercambioGenebraAPI.Application.Commands.UpdateCategory;
+using IntercambioGenebraAPI.Application.Queries.GetAllCategories;
+using IntercambioGenebraAPI.Domain.Entities;
+using IntercambioGenebraAPI.Domain.Repositories;
+using IntercambioGenebraAPI.Infrastructure;
+using IntercambioGenebraAPI.Infrastructure.Repositories;
+using IntercambioGenebraAPI.Tests.Utils.Factories;
+using IntercambioGenebraAPI.Tests.Utils.Factories.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace IntercambioGenebraAPI.Tests.Application.Queries
+{
+    public class GetAllCategoriesQueryHandlerTests
+    {
+        private readonly AppDbContext _context;
+
+        public GetAllCategoriesQueryHandlerTests()
+        {
+            _context = InMemoryContextFactory.Create();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnAllCategoriesRegistered()
+        {
+            CategoryFactory.CreateTestCategory(_context, "Category1");
+            CategoryFactory.CreateTestCategory(_context, "Category2");
+            CategoryFactory.CreateTestCategory(_context, "Category3");
+
+            var query = new GetAllCategoriesQuery();
+            var handler = new GetAllCategoriesQueryHandler(_context);
+            var response = await handler.Handle(query, CancellationToken.None);
+            var result = response?.GetResult().As<OkObjectResult>();
+            var categories = result?.Value.As<List<Category>>();
+
+            categories.Should().NotBeNull().And.HaveCount(3);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnNoCategories_IfNoCategoriesAreRegistered()
+        { 
+            var query = new GetAllCategoriesQuery();
+            var handler = new GetAllCategoriesQueryHandler(_context);
+            var response = await handler.Handle(query, CancellationToken.None);
+            var result = response?.GetResult().As<OkObjectResult>();
+            var categories = result?.Value.As<List<Category>>();
+
+            categories.Should().NotBeNull().And.HaveCount(0);
+        }
+    }
+}
---
+++ IntercambioGenebraAPI.Tests/Application/Queries/GetProductByIdQueryHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using IntercambioGenebraAPI.Application.Commands.UpdateProduct;
+using IntercambioGenebraAPI.Application.Queries.GetAllProducts;
+using IntercambioGenebraAPI.Application.Queries.GetProduct;
+using IntercambioGenebraAPI.Domain.Entities;
+using IntercambioGenebraAPI.Domain.MapperProfiles;
+using IntercambioGenebraAPI.Domain.Repositories;
+using IntercambioGenebraAPI.Domain.ViewModels;
+using IntercambioGenebraAPI.Infrastructure;
+using IntercambioGenebraAPI.Infrastructure.Repositories;
+using IntercambioGenebraAPI.Tests.Utils.Factories;
+using IntercambioGenebraAPI.Tests.Utils.Factories.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace IntercambioGenebraAPI.Tests.Application.Queries
+{
+    public class GetProductByIdQueryHandlerTests
+    {
+        private readonly AppDbContext _context;
+
+        public GetProductByIdQueryHandlerTests()
+        {
+            _context = InMemoryContextFactory.Create();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnProductById()
+        {
+            var testProduct = ProductFactory.CreateTestProduct(_context);
+            var testProductId = testProduct.Id;
+            var query = new GetProductByIdQuery(testProductId);
+            var handler = new GetProductByIdQueryHandler(_context);
+            var response = await handler.Handle(query, CancellationToken.None);
+            var result = response?.GetResult().As<OkObjectResult>();
+            var product = result?.Value.As<ProductViewModel>();
+
+            product.Should().NotBeNull();
+            product!.Id.Should().Be(testProductId);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnNotFound_IfProductDoesNotExist()
+        {
+            var testProductId = new Guid();
+            var query = new GetProductByIdQuery(testProductId);
+            var handler = new GetProductByIdQueryHandler(_context);
+            var response = await handler.Handle(query, CancellationToken.None);
+            
+            response.GetResult().Should().BeOfType<NotFoundObjectResult>();
+        }
+    }
+}
