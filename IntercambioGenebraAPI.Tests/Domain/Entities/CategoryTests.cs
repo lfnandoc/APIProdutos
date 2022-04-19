f00c2b8b3054f85894226869e17bccf1612fcf3b
Luiz Fernando Cardoso <nando.cardoso03@gmail.com>
2022-04-19 15:54:22 -0300

    add domain unit tests

---
+++ IntercambioGenebraAPI.Tests/Domain/Entities/CategoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using FluentAssertions;
+using IntercambioGenebraAPI.Domain.Entities;
+
+namespace IntercambioGenebraAPI.Tests.Domain.Entities
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetProperties()
+        {
+            var testName = "Software";
+            var testCategory = new Category(testName);
+
+            testCategory.Name.Should().Be(testName);
+        }
+
+        [Fact]
+        public void ChangeName_ShouldChangeName()
+        {
+            var testName = "Software";
+            var testCategory = new Category(testName);
+            var newName = "Hardware";
+
+            testCategory.ChangeName(newName);
+            testCategory.Name.Should().Be(newName);
+        }
+    }
+}
---
+++ IntercambioGenebraAPI.Tests/Domain/Entities/ProductTests.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+using FluentAssertions;
+using IntercambioGenebraAPI.Domain.Entities;
+
+namespace IntercambioGenebraAPI.Tests.Domain.Entities
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetProperties()
+        {
+            var testName = "Visual Studio";
+            var testPrice = 10.85m;
+            var testCategory = new Category("Software");
+            var testProduct = new Product(testName, testCategory, testPrice);
+
+            testProduct.Name.Should().Be(testName);
+            testProduct.Price.Should().Be(testPrice);
+            testProduct.Category.Should().Be(testCategory);
+            testProduct.CategoryId.Should().Be(testCategory.Id);
+        }
+
+        [Fact]
+        public void Constructor_ShouldSetPriceAsZero_IfPriceIsNull()
+        {
+            var testName = "Visual Studio";
+            decimal? testPrice = null;
+            var testCategory = new Category("Software");
+            var testProduct = new Product(testName, testCategory, testPrice);
+            
+            testProduct.Price.Should().Be(0);
+        }
+
+        [Fact]
+        public void ChangeName_ShouldChangeName()
+        {
+            var testName = "Visual Studio";
+            var testCategory = new Category("Software");
+            var testProduct = new Product(testName, testCategory);
+            var newName = "Word";
+
+            testProduct.ChangeName(newName);
+            testProduct.Name.Should().Be(newName);
+        }
+
+        [Fact]
+        public void ChangeCategory_ShouldChangeCategory()
+        {
+            var testName = "Visual Studio";
+            var testCategory = new Category("Software");
+            var testProduct = new Product(testName, testCategory);
+            var newCategory = new Category("Hardware");
+
+            testProduct.ChangeCategory(newCategory);
+            testProduct.Category.Should().Be(newCategory);
+            testProduct.CategoryId.Should().Be(newCategory.Id);
+        }
+
+        [Fact]
+        public void ChangePrice_ShouldChangePrice()
+        {
+            var testName = "Visual Studio";
+            var testPrice = 10.85m;
+            var testCategory = new Category("Software");
+            var testProduct = new Product(testName, testCategory, testPrice);
+            var newPrice = 0.99m;
+
+            testProduct.ChangePrice(newPrice);
+            testProduct.Price.Should().Be(newPrice);
+        }
+        
+    }
+}
